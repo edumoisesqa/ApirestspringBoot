b08ba5f848d4c306b6119c4214c2fa1ebe2efc85
eduardomoisesqa <eduardo.silva.cps@elo.com.br>
2021-10-02 15:57:09 -0300

    refactore

--- grade_curricular/src/main/java/com/rasmoo/cliente/escola/grade_curricular/GradeCurricularApplication.java
+++ grade_curricular/src/main/java/com/rasmoo/cliente/escola/grade_curricular/GradeCurricularApplication.java
@@ -2,9 +2,15 @@ package com.rasmoo.cliente.escola.grade_curricular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GradeCurricularApplication {
+public class GradeCurricularApplication extends SpringBootServletInitializer {
+@Override
+protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+	return application.sources(GradeCurricularApplication.class);
+}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeCurricularApplication.class, args);
